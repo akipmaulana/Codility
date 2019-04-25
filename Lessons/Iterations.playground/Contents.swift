d9d1ddbbf3fde70fd1ab463536814873fb9eea18
Akip Maulana <akip.maulana@gmail.com>
2019-04-25 16:51:22 +0700

    Lesson Binary Gap

--- Lessons/Iterations.playground/Contents.swift
+++ Lessons/Iterations.playground/Contents.swift
@@ -1,5 +1,36 @@
+// BINARY GAP
+
 import UIKit
 
-var str = "Hello, playground"
+public func solution(_ N : Int) -> Int {
+    var openGate: Bool = false
+    var closeGate: Bool = false
+    var gapCount: Int = 0
+    var longest: Int = 0
+    
+    let binary = String(N, radix: 2, uppercase: true)
+    print("The binary is \(binary)")
+    
+    for chr in binary {
+        if !openGate {
+            openGate = chr == "1"
+            continue
+        }
+    
+        if openGate && chr == "1" {
+            if gapCount > longest {
+                longest = gapCount
+            }
+            gapCount = 0
+            continue
+        }
+        
+        if openGate && chr == "0" {
+            gapCount += 1
+        }
+    }
+    
+    return longest
+}
 
-print(str)
+print("Longest binary gap is of length \(solution(1162)).")
